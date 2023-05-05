71140bc39684fd652307dad0b46e5e791bb8cd82
brickwall <sk292207@gmail.com>
2023-05-05 20:53:34 +0530

    Add search form for users to search for images with id

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'photos/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :kittens
+  resources :photos, only: [:index] 
   # Defines the root path route ("/")
-  root "kittens#index"
+  root "photos#index"
 end
