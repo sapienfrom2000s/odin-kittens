250063e35310e342b7b7c39cc41a425c2515fada
brickwall <sk292207@gmail.com>
2023-05-06 01:50:20 +0530

    Connect flickr api to the application

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,5 +1,20 @@
 class PhotosController < ApplicationController
   def index
-    Photo
+    if params[:flickr_info]
+      @flickr_info = params_user_id
+      @photos_list = Photo.fetch(@flickr_info[:user_id])
+      if @photos_list
+        render "index"
+      else
+        flash[:error] = "Enter valid user_id" 
+        render "index"
+      end
+    end
+  end
+
+  private
+
+  def params_user_id
+    params.require(:flickr_info).permit(:user_id)
   end
 end
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,3 +1,10 @@
+require 'flickr'
+
 class Photo < ApplicationRecord
-	
+  def self.fetch(user_id)
+  	key = ENV['FLICKR_API_KEY']
+  	secret = ENV['FLICKR_API_KEY']
+  	flickr = Flickr.new(key, secret)
+  	json_data = flickr.photos.search(:user_id => user_id)
+  end
 end
